04dd986a90df5c8ad167b2f9b918bb5c97867277
AmitPress <amitkrsurjo@gmail.com>
2023-10-12 16:16:37 +0530

    change 0.0.0.0 to 127.0.0.1

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('postgresql://amit:12345@0.0.0.0:5432/aslpm')
+engine = create_engine('postgresql://amit:12345@127.0.0.1:5432/aslpm')
 
 conn = engine.connect()
 
